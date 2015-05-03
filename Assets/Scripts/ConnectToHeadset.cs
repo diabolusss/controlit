e20b9c7ffdb8e60c308b6f6ac4065c35eb051698
Forrest Spade <fspade@gmail.com>
2015-05-02 21:20:28 -0700

    Add bathroom and touch to walk

--- Assets/Scripts/ConnectToHeadset.cs
+++ Assets/Scripts/ConnectToHeadset.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class ConnectToHeadset : MonoBehaviour {
 
     Headset headset;
+    GUIStyle style = new GUIStyle();
+
+    void Awake() {
+        style.fontSize = 30;
+        style.normal.textColor = Color.white;
+    }
 
 #if UNITY_ANDROID && !UNITY_EDITOR
 
@@ -21,9 +27,27 @@ public class ConnectToHeadset : MonoBehaviour {
 
 #endif
     void OnGUI() {
-        GUI.Label(new Rect(10, 10, 200, 50), headset.GetConnectString());
-        GUI.Label(new Rect(10, 60, 50, 50), headset.GetMeditation().ToString());
-        GUI.Label(new Rect(10, 110, 50, 50), headset.GetPoorSignalValue().ToString());
+        GUI.Label(new Rect(10, 10, 200, 50), headset.GetConnectString(), style);
+        GUI.Label(new Rect(10, 60, 50, 50), headset.GetMeditation().ToString(), style);
+        GUI.Label(new Rect(10, 110, 50, 50), headset.GetPoorSignalValue().ToString(), style);
+    }
+
+
+    void Update() {
+        CheckMeditation();
+    }
+
+    public Material monsterMat;
+
+    void CheckMeditation() {
+        int meditation = headset.GetMeditation();
+
+        if (meditation >= 50) {
+            monsterMat.SetFloat("_Blend", Mathf.Clamp01(monsterMat.GetFloat("_Blend") + 0.01f));
+        }
+        else  {
+            monsterMat.SetFloat("_Blend", Mathf.Clamp01(monsterMat.GetFloat("_Blend") - 0.01f));
+        }
     }
 
 }
---
+++ Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCharacter : MonoBehaviour {
+
+    public GameObject player;
+    public Animation playerAnim;
+
+    private Vector3 moveTarget;
+    private bool moving = false;
+	
+	// Update is called once per frame
+	void Update () {
+        if (moving == true) {
+            playerAnim.Play("Walk");
+            player.transform.position = Vector3.MoveTowards(player.transform.position, moveTarget, Time.deltaTime * 2);
+        }
+        if (player.transform.position == moveTarget) {
+            moving = false;
+            playerAnim.Play("Stand");
+        }
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1<<8)){
+                moveTarget = hit.point;
+                moving = true;
+            }
+            player.transform.LookAt(moveTarget);
+
+        }
+	}
+}
